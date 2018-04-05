3cf678b751c1311b352a832489e1118edc3060a3
Riccardo Manfrin <name surname at gmail dot com>
2018-04-05 08:12:10 +0000

    Adds samples code used to test former queries

---
+++ samples/check_vgrid.py
@@ -0,0 +1,8 @@
+#!/bin/python
+
+from aurorapy.client import AuroraError, AuroraTCPClient, AuroraSerialClient
+
+
+inverter = AuroraSerialClient(port='/dev/ttyUSB0', address=2, baudrate=19200, data_bits=8, parity='N', stop_bits=1, timeout=0.1, tries=3)
+inverter.connect()
+print inverter.measure(1, global_measure=True)
